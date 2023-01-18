b92f0878c564a998922743ebad4a2e7d907b1ef0
ksquaredcoding <ksquaredcoding@gmail.com>
2023-01-17 18:52:21 -0700

    through heroicon content

--- src/components/TestModal.vue
+++ src/components/TestModal.vue
@@ -3,7 +3,17 @@
     <div class="fixed inset-0 bg-gray-500 bg-opacity-50 transition-opacity"></div>
     <div class="fixed inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-
+        <div
+          class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <div
+                class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-slate-100 sm:mx-0 sm:h-10 sm:w-10">
+                <!-- this div for heroicon -->
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
